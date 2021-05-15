6dc547ec1224f669d198163a634e2579d73a8805
Khunt Yashik <yashikkhunt2002@gmail.com>
2021-05-15 14:22:26 +0530

    ADD: added movie landing pages from database

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,40 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import {
-    Link
-} from "react-router-dom";
+import { useParams } from "react-router-dom"
+import db from "../firebase"
 
 function Detail() {
+
+    const { id } = useParams();
+    const [movie, setmovie] = useState()
+
+    useEffect(() => {
+        //grab the moive from db
+        db.collection("movies")
+            .doc(id)
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    //save the movie data
+                    setmovie(doc.data());
+                } else {
+                    //redirect to home page
+                }
+            })
+    }, [])
+    console.log(movie);
+
     return (
         <Container>
-            <Background>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" alt="" />
-            </Background>
-            <ImageTitle>
-                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt="" />
-            </ImageTitle>
-            <Controls>
-                <PlayButton>
-                    <img src="/images/play-icon-black.png" alt="" />
-                    <span>Play</span>
-                </PlayButton>
-                <TrailerButton>
-                    <img src="/images/play-icon-white.png" alt="" />
-                    <span>Trailer</span>
-                </TrailerButton>
-                <Addbutton>
-                    <span>+</span>
-                </Addbutton>
-                <GroupWatchButton>
-                    <img src="/images/group-icon.png" alt="" />
-                </GroupWatchButton>
-            </Controls>
-            <SubTitle>
-                2018 * 7m * Family, Fantasy, Kids, Animation
-            </SubTitle>
-            <Description>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti quae ab quibusdam vitae assumenda quidem beatae esse iste. Vel dolor, nisi atque aut tempora fugit nihil dignissimos consequatur earum!
-            </Description>
+            {movie && (<>
+                <Background>
+                    <img src={movie.backgroundImg} alt="" />
+                </Background>
+                <ImageTitle>
+                    <img src={movie.titleImg} alt="" />
+                </ImageTitle>
+                <Controls>
+                    <PlayButton>
+                        <img src="/images/play-icon-black.png" alt="" />
+                        <span>Play</span>
+                    </PlayButton>
+                    <TrailerButton>
+                        <img src="/images/play-icon-white.png" alt="" />
+                        <span>Trailer</span>
+                    </TrailerButton>
+                    <Addbutton>
+                        <span>+</span>
+                    </Addbutton>
+                    <GroupWatchButton>
+                        <img src="/images/group-icon.png" alt="" />
+                    </GroupWatchButton>
+                </Controls>
+                <SubTitle>
+                    {movie.subTitle}
+                </SubTitle>
+                <Description>
+                    {movie.description}
+                </Description>
+                </>)
+            }
         </Container>
     )
 }
@@ -81,6 +103,7 @@ const ImageTitle = styled.div`
 const Controls = styled.div`
     display: flex;
     align-items: center;
+    margin-top: 30px;
 `
 
 const PlayButton = styled.button`
